a16e887cd9035b4397e1ef30be2b373aa7a2e7e3
Manuel Nunez <manuelandy@gmail.com>
2017-05-27 02:05:34 -0400

    bien

---
+++ GameRasp/main/Settings.py
@@ -0,0 +1,23 @@
+#Options
+Title="KLK"
+WIDTH=360
+HEIGHT=480
+FPS=30
+#Player sett
+PLAYER_ACC=0.5
+PLAYER_FRICTION=-0.12
+PLAYER_GRAV= 1
+PLAYER_JUMP=22
+
+#Plataformas formato (x,y,thicknes)
+PLATFORM_LIST=[(0, HEIGHT-40,WIDTH,40),
+               (WIDTH/2-50,HEIGHT*3/4,100,20),
+               (350,200,100,20),
+               (175,100,50,20)]
+
+#colors
+WHITE= (255,255,255)
+BLACK= (0,0,0)
+RED=(255,0,0)
+GREEEN=(0,255,0)
+BLUE=(0,0,255)
